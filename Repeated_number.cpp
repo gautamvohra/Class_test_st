64394b1fa229cb4ef1b06def48682d548f5f0f8a
gautamvohra <73735386+gautamvohra@users.noreply.github.com>
2022-08-26 10:19:25 +0530

    Added code for Repeated Problem

---
+++ Repeated_number.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//implementing the logic
+void findRepeat(int* a, int n){
+    unordered_map<int,int> hash;
+    
+    for(int i=0;i<n;i++){
+        hash[a[i]]++;
+    }
+    
+    cout<<"repeated Number    frequency\n";
+   
+    for(auto it=hash.begin();it!=hash.end();it++)
+    if(it->second>1)
+    cout<<it->first<<"                  "<<it->second<<endl;
+    
+}
+
+int main()
+{   //taking input
+    int n;
+    cout<<"enter array length\n";
+    cin>>n;
+    int* a=(int*)(malloc(sizeof(int)*n));
+    
+    cout<<"input array elements...\n";
+    
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    //calling the function created
+    findRepeat(a,n);
+
+    return 0;
+}
